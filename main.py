d2c16c1d4ef6b51e214987c41574507db568cd40
WangYT0404 <161801491+WangYT0404@users.noreply.github.com>
2024-05-29 23:35:23 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+#main.py
+
+import simpy
+from Node import Node
+
+env=simpy.Environment()
+
+band_width=200
+file_num=1
+packet_size=10#bit
+window_size=4
+timer=0.5#s
+sleep_interval=0.02
+
+node0=Node(env,name=0,timer=timer,sleep_interval=sleep_interval,
+           band_width=band_width,window_size=window_size,packet_size=packet_size)
+node1=Node(env,name=1,timer=timer,sleep_interval=sleep_interval,
+           band_width=band_width,window_size=window_size,packet_size=packet_size)
+node2=Node(env,name=2,timer=timer,sleep_interval=sleep_interval,
+           band_width=band_width,window_size=window_size,packet_size=packet_size)
+node3=Node(env,name=3,timer=timer,sleep_interval=sleep_interval,
+           band_width=band_width,window_size=window_size,packet_size=packet_size)
+nodes=[node0,node1,node2,node3]
+
+data = "11111111111111111122222222222222222222222333333333333333333"
+env.process(node0.source_send(data=data,start_time=0.4,file_id=0,des_node=node1,nodes=nodes))
+env.process(node2.source_send(data=data,start_time=0,file_id=1,des_node=node3,nodes=nodes))
+env.process(node0.source_send(data=data,start_time=0.2,file_id=2,des_node=node3,nodes=nodes))
+env.run(until=50)
+
+
+print('node1[',0,']:')
+for pkt in node1.packets[0]:
+    print(pkt.packet_id)
+print('node3[',1,']:')
+for pkt in node3.packets[1]:
+    print(pkt.packet_id)
+print('node3[',2,']:')
+for pkt in node3.packets[2]:
+    print(pkt.packet_id)
